76a60d08a8e58ad1212e14d05e7735280f2eb87b
stamatak <Alexandros.Stamatakis@gmail.com>
2014-12-15 17:07:38 +0100

    updated version header once more

--- versionHeader/version.h
+++ versionHeader/version.h
@@ -1,4 +1,4 @@
 #define programName        "ExaML"
-#define programVersion     "3.0.6"
-#define programVersionInt  306
+#define programVersion     "3.0.7"
+#define programVersionInt  307
 #define programDate        "December 15 2014"
